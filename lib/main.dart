fb66ce04e4d180d80c1d8ef6d2cbe915e3d5e04a
ahadahmed9980 <abdul.ahad05112006@gmail.com>
2026-08-12 18:35:18 +0500

    Update code for main branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart'; 
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform, 
+    );
+  } catch (e) {
+    debugPrint(e.toString());
+  }
+
   runApp(const MyApp());
 }
 
